69c30850bc83a729157b252164970cd7134a1137
urmasa <nik.goltsev.97@bk.ru>
2025-04-19 14:27:53 +0300

    registration

--- madiminecraft/backend/madiminecraft/settings.py
+++ madiminecraft/backend/madiminecraft/settings.py
@@ -55,10 +55,13 @@ ROOT_URLCONF = 'madiminecraft.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),  # Вот эта строка важна
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
--- madiminecraft/backend/ratings/views.py
+++ madiminecraft/backend/ratings/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import PlayerStats
 
-# Create your views here.
+def player_ratings(request):
+    # Получаем статистику игроков, сортируем по убийствам (kills)
+    stats = PlayerStats.objects.select_related('user').all().order_by('-kills')
+    return render(request, 'ratings/ratings.html', {'stats': stats})
